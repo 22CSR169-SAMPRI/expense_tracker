073b631ebeaa48ec47281b8bfe2e0bac8e664bed
22CSR169-SAMPRI <166939061+22CSR169-SAMPRI@users.noreply.github.com>
2024-11-06 10:58:05 +0530

    base url

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -2,9 +2,9 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = "https://expense-tracker-2-s91b.onrender.com";
+const BASE_URL = "http://localhost:5000/api/v1/";
+// https://expense-tracker-2-s91b.onrender.com
 
-// http://localhost:5000/api/v1/
 const GlobalContext = React.createContext()
 
 export const GlobalProvider = ({children}) => {
